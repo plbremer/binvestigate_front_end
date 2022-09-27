6c42d5e3c8e5e029b46fb3622c97e175f3088b78
rictuar <parkerbremer@gmail.com>
2022-09-27 14:46:55 -0700

    leaf table initial pass

--- newer_frontend/pages/differential_analysis.py
+++ newer_frontend/pages/differential_analysis.py
@@ -3,8 +3,14 @@ from dash import dcc, html, dash_table, callback
 import plotly.express as px
 import dash_bootstrap_components as dbc
 from . import venn_helper
+import requests
+from dash.dependencies import Input, Output, State
+import pandas as pd
+from dash_table.Format import Format, Scheme, Group
 
 dash.register_page(__name__)
+
+base_url_api = "http://127.0.0.1:4999/"
 
 ########get things from helper script########
 unique_sod_combinations_dict=venn_helper.get_unique_sod_combinations()
@@ -34,7 +40,7 @@ layout = dbc.Container(children=[
                         options=sorted([
                             {'label': temp.title(), 'value':unique_sod_combinations_dict[temp]} for temp in unique_sod_combinations_dict
                         ],key=lambda x:x['label']),
-                        multi=True,
+                        multi=False,
                         # style={
                         #     'color': '#212121',
                         #     'background-color': '#3EB489',
@@ -61,7 +67,7 @@ layout = dbc.Container(children=[
                         options=sorted([
                             {'label': temp.title(), 'value':unique_sod_combinations_dict[temp]} for temp in unique_sod_combinations_dict
                         ],key=lambda x:x['label']),
-                        multi=True,
+                        multi=False,
                         # style={
                         #     'color': '#212121',
                         #     'background-color': '#3EB489',
@@ -105,11 +111,11 @@ layout = dbc.Container(children=[
                             dbc.RadioItems(
                                 id='radio_items_fold_type',
                                 options=[
-                                    {'label': 'Knowns', 'value': 'knowns'},
-                                    {'label': 'Classes', 'value': 'classes'},
-                                    {'label': 'Unknowns', 'value': 'unknowns'},
+                                    {'label': 'Knowns', 'value': 'known'},
+                                    {'label': 'Classes', 'value': 'class'},
+                                    {'label': 'Unknowns', 'value': 'unknown'},
                                 ],         
-                                value='knowns',
+                                value='known',
                                 className="btn-group",
                                 inputClassName="btn-check",
                                 labelClassName="btn btn-outline-primary",
@@ -225,10 +231,11 @@ layout = dbc.Container(children=[
                         id='leaf_table',
                         columns=[
                             {"name": "English Name", "id": "english_name"},
-                            {"name": "Fold Average", "id": "fold_average"},
-                            {"name": "Significance Welch", "id": "sig_welch"},
-                            {"name": "Fold Median", "id": "fold_median"},
-                            {"name": "Significance MWU", "id": "sig_mannwhit"}
+                            {"name": "Identifier", "id": "identifier"},
+                            {"name": "Fold Average", "id": "fold_change_average","type": "numeric","format": Format(group=Group.yes, precision=2, scheme=Scheme.exponent)},
+                            {"name": "Significance Welch", "id": "significance_welch","type": "numeric","format": Format(group=Group.yes, precision=2, scheme=Scheme.exponent)},
+                            {"name": "Fold Median", "id": "fold_change_median","type": "numeric","format": Format(group=Group.yes, precision=2, scheme=Scheme.exponent)},
+                            {"name": "Significance MWU", "id": "significance_mwu","type": "numeric","format": Format(group=Group.yes, precision=2, scheme=Scheme.exponent)}
                         ],
                         data=[],
                         page_current=0,
@@ -261,3 +268,55 @@ layout = dbc.Container(children=[
         #justify='center'
     ),
 ])
+
+
+
+
+
+@callback(
+    [
+        #Output(component_id="leaf_table", component_property="columns"),
+        Output(component_id="leaf_table", component_property="data")
+    ],
+    [
+        Input(component_id='leaf_query', component_property='n_clicks'),
+    ],
+    [
+        State(component_id='dropdown_triplet_selection_from',component_property='value'),
+        State(component_id='dropdown_triplet_selection_to',component_property='value'),
+        State(component_id='radio_items_fold_type',component_property='value')
+    ],
+    prevent_initial_call=True
+)
+def query_table(leaf_query_n_clicks,dropdown_triplet_selection_from_value,dropdown_triplet_selection_to_value,radio_items_fold_type_value):
+
+    leaf_output={
+        "triplet_from":dropdown_triplet_selection_from_value,
+        "triplet_to":dropdown_triplet_selection_to_value
+    }
+
+    response = requests.post(base_url_api + "/leafresource/", json=leaf_output)
+    total_panda = pd.read_json(response.json(), orient="records")
+    print(total_panda)
+    # total_panda['binvestigate']='binvestigate'
+
+    # if radio_items_sunburst_value_value=='intensity_average':
+    #     last_column={'name': 'Average Intensity','id':'intensity_average',"type": "numeric","format": Format(group=Group.yes, precision=2, scheme=Scheme.exponent)}
+    # elif radio_items_sunburst_value_value=='intensity_median':
+    #     last_column={'name': 'Median Intensity','id':'intensity_median',"type": "numeric","format": Format(group=Group.yes, precision=2, scheme=Scheme.exponent)}
+    # elif radio_items_sunburst_value_value=='percent_present':
+    #     last_column={'name': 'Percent Present','id':'percent_present',"type": "numeric","format": Format(group=Group.yes, precision=2, scheme=Scheme.exponent)}
+    # column_list=[
+    #     {'name':'Species','id':'species'},
+    #     {'name':'Organ','id':'organ'},
+    #     {'name':'Disease','id':'disease'},
+    #     #{'name':'Species','id':'species'},
+    #     last_column
+    # ]
+    # total_panda=total_panda[['binvestigate','species','organ','disease',last_column['id']]]
+
+    total_panda=total_panda.loc[total_panda['bin_type_dict']==radio_items_fold_type_value]
+
+    data = total_panda.to_dict(orient='records')
+
+    return [data]
