14031892ab19ff9293abdb7be3781c8c1ecee415
rictuar <parkerbremer@gmail.com>
2022-10-21 10:29:23 -0700

    major directory cleanup

--- new_frontend/dash_in_flask_msal/config.py
+++
@@ -1,6 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    BRAND = os.environ.get('Super Duper Comparison Thingy') or 'Super Duper Comparison Thingy'
-
