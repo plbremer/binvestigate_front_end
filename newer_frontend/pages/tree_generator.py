3181f91944c1af04469acb8a58bc2b1234146b25
rictuar <parkerbremer@gmail.com>
2022-12-13 17:24:26 -0800

    tree metadata working

--- newer_frontend/pages/tree_generator.py
+++ newer_frontend/pages/tree_generator.py
@@ -11,10 +11,11 @@ from . import hierarchical_differential_analysis_helper
 import networkx as nx
 from . import venn_helper
 
+from pprint import pprint
 dash.register_page(__name__)
 
-base_url_api = f"http://api_alias:4999/"
-
+#base_url_api = f"http://api_alias:4999/"
+base_url_api = f"http://127.0.0.1:4999/"
 
 ########get things from helper script########
 species_networkx,species_node_dict=hierarchical_differential_analysis_helper.extract_networkx_selections_species()
@@ -97,7 +98,7 @@ layout=html.Div(
                     children=[
                         #html.H2("From Triplet", className='text-center'),
                         dcc.Dropdown(
-                            id='dropdown_triplet_selection_from',
+                            id='dropdown_triplet_selection',
                             options=sorted([
                                 {'label': temp, 'value':unique_sod_combinations_dict[temp]} for temp in unique_sod_combinations_dict
                             ],key=lambda x:x['label']),
@@ -218,48 +219,124 @@ layout=html.Div(
 )
 
 
+@callback(
+    [
+        Output(component_id="dropdown_species", component_property="options"),
+        Output(component_id="dropdown_organ", component_property="options"),
+        Output(component_id="dropdown_disease", component_property="options"),
+    ],
+    [
+        Input(component_id="dropdown_species", component_property="value"),
+        Input(component_id="dropdown_organ", component_property="value"),
+        Input(component_id="dropdown_disease", component_property="value"),
+    ],
+    prevent_initial_call=True
+)
+def update_input_options_to(
+    to_species_value_input,
+    to_organ_value_input,
+    to_disease_value_input,
+):
+
+    '''
+    this callback makes it so that if a user specifies a species, an organ, or a disease
+    for "to", then the other options are filtered accordingly
+    '''
+    #print('here')
+    #determine valid species options
+    temp_view=index_panda.copy()
+    if to_species_value_input!=None:
+        temp_set=nx.algorithms.dag.descendants(species_networkx,to_species_value_input)
+        temp_set.add(to_species_value_input)
+        temp_view=temp_view.loc[
+            temp_view.species.isin(temp_set)
+        ]
+
+    if to_organ_value_input!=None:
+        temp_set=nx.algorithms.dag.descendants(organ_networkx,to_organ_value_input)
+        temp_set.add(to_organ_value_input)
+        temp_view=temp_view.loc[
+            temp_view.organ.isin(temp_set)
+        ]
+
+    if to_disease_value_input!=None:
+        #print(to_disease_value_input)
+        temp_set=nx.algorithms.dag.descendants(disease_networkx,to_disease_value_input)
+        temp_set.add(to_disease_value_input)
+        temp_view=temp_view.loc[
+            temp_view.disease.isin(temp_set)
+        ]
+
+    all_basic_species_options=set(temp_view.species.values)
+    all_valid_species_options=all_basic_species_options
+    [all_valid_species_options:=all_valid_species_options.union(nx.ancestors(species_networkx,temp_option)) for temp_option in all_basic_species_options]
+
+    all_basic_organ_options=set(temp_view.organ.values)
+    all_valid_organ_options=all_basic_organ_options
+    [all_valid_organ_options:=all_valid_organ_options.union(nx.ancestors(organ_networkx,temp_option)) for temp_option in all_basic_organ_options]
+
+    all_basic_disease_options=set(temp_view.disease.values)
+    all_valid_disease_options=all_basic_disease_options
+    [all_valid_disease_options:=all_valid_disease_options.union(nx.ancestors(disease_networkx,temp_option)) for temp_option in all_basic_disease_options]
+
+    species_options=sorted([
+        {'label':species_node_dict[temp], 'value':temp.title()} for temp in species_node_dict if temp in all_valid_species_options
+    ],key=lambda x:x['label'])
+
+    organ_options=sorted([
+        {'label':organ_node_dict[temp], 'value':temp} for temp in organ_node_dict if temp in all_valid_organ_options
+    ],key=lambda x:x['label'])
+
+    disease_options=sorted([
+        {'label':disease_node_dict[temp], 'value':temp} for temp in disease_node_dict if temp in all_valid_disease_options
+    ],key=lambda x:x['label'])
+
+    return species_options,organ_options,disease_options
 
 
-# @callback(
-#     [
-#         #Output(component_id="leaf_table", component_property="columns"),
-#         Output(component_id="tree_table_metadata", component_property="data")
-#     ],
-#     [
-#         Input(component_id="tree_metadata_query", component_property="n_clicks"),
-#     ],
-#     [
-#         State(component_id="dropdown_species", component_property="value"),
-#         State(component_id="dropdown_organ", component_property="value"),
-#         State(component_id="dropdown_disease", component_property="value"),
-#     ],
-#     prevent_initial_call=True
-# )
-# def perform_metadata_query(
-#     query_n_clicks,
-#     species_value,
-#     organ_value,
-#     disease_value,
-# ):
-#     '''
-#     describes the query that the user makes
-#     '''
-#     ################metadata query######################
-#     #prepare json for api
-#     metadata_json_output = {
-#         "from_species": from_species_value,
-#         "from_organ": from_organ_value,
-#         "from_disease": from_disease_value,
-#         "to_species": to_species_value,
-#         "to_organ": to_organ_value,
-#         "to_disease": to_disease_value,
-#     }
-#     #obtain results from api
-#     response = requests.post(base_url_api + "/treemetadataresource/", json=metadata_json_output)
-#     total_panda = pd.read_json(response.json(), orient="records")
 
-#     #print(total_panda)
+@callback(
+    [
+        #Output(component_id="leaf_table", component_property="columns"),
+        Output(component_id="tree_table_metadata", component_property="data")
+    ],
+    [
+        Input(component_id="tree_metadata_query", component_property="n_clicks"),
+    ],
+    [
+        State(component_id="dropdown_species", component_property="value"),
+        State(component_id="dropdown_organ", component_property="value"),
+        State(component_id="dropdown_disease", component_property="value"),
+        State(component_id="dropdown_triplet_selection", component_property="value"),
+    ],
+    prevent_initial_call=True
+)
+def perform_metadata_query(
+    query_n_clicks,
+    dropdown_species_value,
+    dropdown_organ_value,
+    dropdown_disease_value,
+    dropdown_triplet_selection_value
 
-#     data = total_panda.to_dict(orient='records')
+):
+    '''
+    describes the query that the user makes
+    '''
+    ################metadata query######################
+    #prepare json for api
+    metadata_json_output = {
+        "species": dropdown_species_value,
+        "organ": dropdown_organ_value,
+        "disease": dropdown_disease_value,
+        "species_organ_disease":dropdown_triplet_selection_value
+    }
+    pprint(metadata_json_output)
+    #obtain results from api
+    response = requests.post(base_url_api + "/treemetadataresource/", json=metadata_json_output)
+    total_panda = pd.read_json(response.json(), orient="records")
 
-#     return [data]
+    print(total_panda)
+
+    data = total_panda.to_dict(orient='records')
+
+    return [data]
